74abd7fcbb902f0deb5ca65eddaab31d7b44831d
soohyun-lee <soohyun527@gmail.com>
2020-11-25 21:55:21 +0900

    알고리즘 연습 문제

---
+++ algorithm_4.py
@@ -0,0 +1,12 @@
+# 00시 00분 00초부터 h시 59분 59초까지의 모든 시작 중에서 3이 하나라도 포함되는 모든 경우의 수 출력 
+
+h = int(input())
+
+count = 0
+for i in range(h+1):
+    for j in range(60):
+        for k in range(60):
+            if '3' in str(i) + str(j) + str(k):
+                count += 1
+
+print(count)
