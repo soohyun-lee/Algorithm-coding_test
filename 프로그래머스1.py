db0a8ee429940c7e6a84329de08929fb718d2b89
soohyun-lee <soohyun527@gmail.com>
2020-12-08 01:24:20 +0900

    알고리즘 연습 문제

---
+++ 프로그래머스1.py
@@ -0,0 +1,14 @@
+# 프로그래머스 실패율 계산하기
+
+def solution(N, stages):
+    result = {}
+    stages_len = len(stages)
+    for stage in range(1, N+1):
+        if stages_len != 0:
+            count = stages.count(stage)
+            result[stage] = count / stages_len
+            stages_len -= count
+        else:
+            result[stage] = 0
+    return sorted(result, key=lambda x: result[x], reverse=True)
+
