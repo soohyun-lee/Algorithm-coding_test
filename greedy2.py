1a3ccd4b8dbcd07e48f62b3ad11c1d8692a749a8
soohyun-lee <soohyun527@gmail.com>
2020-12-08 16:11:50 +0900

    알고리즘 연습 문제

---
+++ greedy2.py
@@ -0,0 +1,16 @@
+n = int(input())
+people_list = []
+
+for i in range(n):
+    h, l = map(int, input().split())
+    people_list.append((h, l))
+people_list.sort(reverse=True)
+max_l = 0
+cnt = 0
+for x, y in people_list:
+    if y > max_l:
+        max_l = y
+        cnt += 1     
+print(cnt)
+
+
--- greedy_algorithm_3.py
+++ greedy_algorithm_3.py
@@ -12,8 +12,8 @@ while n >= k:
     n //= k
     result += 1
 
-while n > 1:
-    n -= 1
-    result += 1
+# while n > 1:
+#     n -= 1
+#     result += 1
 
 print(result)
