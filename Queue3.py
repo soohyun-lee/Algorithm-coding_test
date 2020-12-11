c133ebcdf1921b4a86744fdca5ef06a8d4cfbc38
soohyun-lee <soohyun527@gmail.com>
2020-12-11 15:52:27 +0900

    알고리즘 연습 문제

---
+++ Queue3.py
@@ -0,0 +1,18 @@
+from collections import deque
+
+n = input()
+cnt = int(input())
+
+for i in range(cnt):
+    plan = input()
+    dq = deque(n)
+    for x in plan:
+        if x in dq:
+            if x != dq.popleft():
+                print('NO')
+                break
+    else:
+        if len(dq) == 0:
+            print('YES')
+        else:
+            print('NO')
