7087f53c055dd79b8ac0db312bc055cffb67f576
soohyun-lee <soohyun527@gmail.com>
2020-12-12 13:52:26 +0900

    알고리즘 연습 문제

--- dictionaryhash.py
+++ dictionaryhash.py
@@ -21,3 +21,21 @@ for y in str1.keys():
         break
 else:
     print('yes')
+
+
+# 수정 코드
+a = input()
+b = input()
+str1 = dict()
+
+for x in a:
+    str1[x] = str1.get(x, 0) + 1
+for x in b:
+    str1[x] = str1.get(x, 0) - 1
+
+for x in a:
+    if str1.get(x) > 0:
+        print('no')
+        break
+else:
+    print('yes')
