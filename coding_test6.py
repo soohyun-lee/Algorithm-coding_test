df4846d7f86013a704a20872942fa9c73b6e35f7
soohyun-lee <soohyun527@gmail.com>
2021-04-01 23:39:10 +0900

    coding test

---
+++ coding_test6.py
@@ -0,0 +1,15 @@
+n = int(input()) 
+costs = [0 for _ in range(n+1)] 
+
+# print('costs : ', costs)
+
+for i in range(1, n+1): 
+    costs[i] = list(map(int, input().split())) 
+    print('costs : ', costs[i])
+for i in range(2, n+1): 
+    costs[i][0] = costs[i][0] + min(costs[i-1][1], costs[i-1][2]) 
+    costs[i][1] = costs[i][1] + min(costs[i-1][0], costs[i-1][2]) 
+    costs[i][2] = costs[i][2] + min(costs[i-1][0], costs[i-1][1]) 
+
+print(min(costs[n][0], costs[n][1], costs[n][2]))
+
