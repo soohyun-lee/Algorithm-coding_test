9d091c36bb95e05a4b4e789f8fa23246d605bd67
soohyun-lee <soohyun527@gmail.com>
2021-04-01 23:40:09 +0900

    coding test

--- coding_test6.py
+++ coding_test6.py
@@ -6,6 +6,7 @@ costs = [0 for _ in range(n+1)]
 for i in range(1, n+1): 
     costs[i] = list(map(int, input().split())) 
     print('costs : ', costs[i])
+    
 for i in range(2, n+1): 
     costs[i][0] = costs[i][0] + min(costs[i-1][1], costs[i-1][2]) 
     costs[i][1] = costs[i][1] + min(costs[i-1][0], costs[i-1][2]) 
