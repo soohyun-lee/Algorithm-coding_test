ef77e25a4ee332b0b1465ccb27c623ef939e8d18
soohyun Lee <soohyunlee@soohyunui-MacBookPro.local>
2023-03-07 13:02:40 +0900

    coding test

---
+++ coding_test41.py
@@ -0,0 +1,17 @@
+from itertools import combinations
+from collections import Counter
+
+def solution(orders, course):
+    answer = []
+    for c in course:
+        temp = []
+        for order in orders:
+            combine = combinations(sorted(order), c)
+            temp += combine
+        
+        counter = Counter(temp)
+        
+        if len(counter) != 0 and max(counter.values()) != 1:
+            answer += [''.join(f) for f in counter if counter[f] == max(counter.values())]
+
+    return sorted(answer)
